d7efb536793acad108e7e81bab2ee15eb09e32c4
petervanya <peter.vanya@gmail.com>
2019-12-28 21:18:04 +0100

    Improved reading velocities

--- pycba/road_cba.py
+++ pycba/road_cba.py
@@ -68,67 +68,111 @@ class RoadCBA(DataContainer):
 
 
     def read_project_inputs(self,
-                            road_params,
-                            capex,
-                            intensities_0,
-                            intensities_1
+                            df_road_params,
+                            df_capex,
+                            df_int_0,
+                            df_int_1,
+                            verbose=False
                             ):
+        if verbose:
+            print("Reading project inputs...")
         """Read the dataframes
         * road parameters
         * capital investment (CAPEX)
         * intensities in 0th variant
         * intensities in 1st variant
         """
-        self.R = road_params
-        self.C_fin = capex
-        self.I0 = intensities_0
-        self.I1 = intensities_1
+        self.R = df_road_params
+        self.C_fin = df_capex
+        self.I0 = df_int_0
+        self.I1 = df_int_1
         # VERIFY INTEGRITY AND CONSISTENCY OF INPUTS
 
     
-    def read_from_csv(self,
+    def read_project_inputs_csv(self,
                       file_road_params,
                       file_capex,
-                      file_intensities_0,
-                      file_intensities_1
+                      file_int_0,
+                      file_int_1,
+                      verbose=False
                       ):
-        """Read the dataframes from files."""
+        # VERIFY EXTENSIONS
+        if verbose:
+            print("Reading project inputs from csv...")
         self.R = pd.read_csv(file_road_params, index_col=0)
         self.C_fin = pd.read_csv(file_capex, index_col=0)
-        self.I0 = pd.read_csv(file_intensities_0)
+        self.I0 = pd.read_csv(file_int_0)
         self.I0.set_index(["id_section", "vehicle"], inplace=True)
-        self.I1 = pd.read_csv(file_intensities_1)
+        self.I1 = pd.read_csv(file_int_1)
         self.I1.set_index(["id_section", "vehicle"], inplace=True)
         # VERIFY INTEGRITY AND CONSISTENCY OF INPUTS
 
 
+    def read_project_inputs_excel(self, file_xls, verbose=False):
+        # VERIFY EXTENSION
+        if verbose:
+            print("Reading project inputs from xls/xlsx...")
+        xls = pd.ExcelFile(file_xls)
+        self.R = xls.parse("road_params", index_col=0)
+        self.C_inv = xls.parse("capex_fin", index_col=0)
+        self.I0 = xls.parse("intensities_0")
+        self.I0.set_index(["id_section", "vehicle"], inplace=True)
+        self.I1 = xls.parse("intensities_1")
+        self.I1.set_index(["id_section", "vehicle"], inplace=True)
+        # VERIFY INTEGRITY AND CONSISTENCY OF INPUTS
+
+
+    def read_intensities(self, df_int_0, df_int_1):
+        pass
+
+
+    def read_intensities_csv(self, csv_int_0, csv_int_1):
+        pass
+
+    
+    def read_intensities_excel(self, xls_int):
+        pass
+
+
     def read_velocities(self,
-                        file_vel_0, 
-                        file_vel_1,
-                        verbose=False):
+                        df_vel_0,
+                        df_vel_1,
+                        verbose=False
+                        ):
+        if verbose:
+            print("Loading velocitiies...")
+        self.V0 = df_vel_0
+        self.V1 = df_vel_1
+
+
+    def read_velocities_csv(self,
+                            csv_vel_0, 
+                            csv_vel_1,
+                            verbose=False
+                            ):
         """Read the dataframe of velocities ordered by project section
         and vehicle category"""
-        if file_vel_0[-3:] != "csv" or file_vel_1[-3:] != "csv":
-            # RAISE ERROR
+        if csv_vel_0[-3:] != "csv" or csv_vel_1[-3:] != "csv":
             print("One of files does not have required extension: csv.")
 
         if verbose:
-            print("Reading csv...")
-        self.V0 = pd.read_csv(file_vel_0)
+            print("Reading velocities from csv...")
+        self.V0 = pd.read_csv(csv_vel_0)
         self.V0.set_index(["id_section", "vehicle"], inplace=True)
-        self.V1 = pd.read_csv(file_vel_1)
+        self.V1 = pd.read_csv(csv_vel_1)
         self.V1.set_index(["id_section", "vehicle"], inplace=True)
 
 
-    def read_velocities_excel(self, file_vel, verbose=False):
+    def read_velocities_excel(self, xls_vel, verbose=False):
         """Read the dataframe of velocities from one excel file."""
-        if not file_vel.split(".")[-1] in ["xls", "xlsx"]:
+        if not xls_vel.split(".")[-1] in ["xls", "xlsx"]:
             print("File does not have the required extension: xls, xlsx.")
+
         if verbose:
-            print("Reading xls/xlsx...")
-        self.V0 = pd.read_excel(file_vel, sheet_name="velocities_0")
+            print("Reading velocities xls/xlsx...")
+        self.V0 = pd.read_excel(xls_vel, sheet_name="velocities_0")
         self.V0.set_index(["id_section", "vehicle"], inplace=True)
-        self.V1 = pd.read_excel(file_vel, sheet_name="velocities_1")
+        self.V1 = pd.read_excel(xls_vel, sheet_name="velocities_1")
         self.V1.set_index(["id_section", "vehicle"], inplace=True)
 
 

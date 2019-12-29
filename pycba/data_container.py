1af3f9a66a30dab197713c412ce769e240f188f1
petervanya <peter.vanya@gmail.com>
2019-12-29 23:52:46 +0100

    Reorganisation and cleaning

--- pycba/data_container.py
+++ pycba/data_container.py
@@ -18,19 +18,19 @@ class DataContainer(object):
         
         self.df_raw = {}
         self.df_clean = {}
-        self.TM = {}
         self.TM_fin = {}
-
-        self.read_data()
-
-
-    def __str__(self):
-        """Print the list of stored dataframes"""
-        return "\n".join([s for s in self.__dir__() if s[:2] == "df"])
+        self.TM_eco = {}
 
 
-    def read_data(self):
+#    def __dir__(self):
+#        return [attr for attr in dir(self) if not attr[:2] == "__"]
+
+
+    def read_data(self, verbose=False):
         """Read in all the relevant data"""
+        if verbose:
+            print("Reading CBA data...")
+
         # macro data
         self.gdp_growth = pd.read_csv(self.dirn + "gdp_growth.csv", \
             index_col="year")
@@ -41,8 +41,10 @@ class DataContainer(object):
             pd.read_csv(self.dirn + "operation_cost.csv", index_col=0)
         self.df_raw["res_val"] = \
             pd.read_csv(self.dirn + "residual_value.csv", index_col=0)
+        self.df_raw["c_fuel"] =\
+            pd.read_csv(self.dirn + "fuel_cost.csv", index_col=0)
 
-        # physical
+        # physical data
         self.fuel_rho = \
             pd.read_csv(self.dirn + "fuel_density.csv", index_col="fuel")
 
@@ -61,8 +63,6 @@ class DataContainer(object):
             pd.read_csv(self.dirn + "voc.csv", index_col=0)
         self.df_raw["r_fuel"] =\
             pd.read_csv(self.dirn + "fuel_consumption.csv", index_col=0)
-        self.df_raw["c_fuel"] =\
-            pd.read_csv(self.dirn + "fuel_cost.csv", index_col=0)
         self.df_raw["r_acc"] =\
             pd.read_csv(self.dirn + "accident_rate.csv", index_col=0)
         self.df_raw["c_acc"] =\
@@ -77,53 +77,48 @@ class DataContainer(object):
             pd.read_csv(self.dirn + "emission_cost.csv", index_col=0)
         self.df_raw["noise"] =\
             pd.read_csv(self.dirn + "noise.csv", index_col=0)
-        
-
-#    def adjust_gdp_growth(self):
-#        """Create new column with GDP indexing"""
-#        self.gdp_growth["gdp_growth_index"] = self.gdp_growth.gdp_growth + 1.0
 
 
-    def adjust_cpi(self, infl=0.02, N_bw=20, N_fw=30, verbose=False):
+    def adjust_cpi(self, infl=0.02, yr_min=2000, yr_max=2050, verbose=False):
+#    def adjust_cpi(self, infl=0.02, N_bw=20, N_fw=30, verbose=False):
         """Fill in mising values and compute cumulative inflation 
         to be able to adjust the price level"""
-        min_yr = self.cpi.index.min()
-        max_yr = self.cpi.index.max()
         if verbose:
-            print("Values between years %i and %i" % (min_yr, max_yr))
-        v_bw = arange(min_yr - N_bw, min_yr)
-        v_fw = arange(max_yr + 1, max_yr + N_fw + 1)
+            print("Adjusting CPI...")
 
-        self.cpi = pd.concat([
-            pd.DataFrame({"cpi": infl*ones_like(v_bw), "year": v_bw}),
-            self.cpi.reset_index(),
-            pd.DataFrame({"cpi": infl*ones_like(v_fw), "year": v_fw}),
-            ], sort=True)
-        self.cpi.set_index("year", inplace=True)
+#        min_yr = self.cpi.index.min()
+#        max_yr = self.cpi.index.max()
+#        if verbose:
+#            print("Values between years %i and %i" % (min_yr, max_yr))
+#        v_bw = arange(min_yr - N_bw, min_yr)
+#        v_fw = arange(max_yr + 1, max_yr + N_fw + 1)
+#
+#        self.cpi = pd.concat([
+#            pd.DataFrame({"cpi": infl*ones_like(v_bw), "year": v_bw}),
+#            self.cpi.reset_index(),
+#            pd.DataFrame({"cpi": infl*ones_like(v_fw), "year": v_fw}),
+#            ], sort=True)
+#        self.cpi.set_index("year", inplace=True)
 
-        self.cpi["cpi2"] = self.cpi.cpi + 1.0
+        self.cpi = self.cpi.reindex(arange(yr_min, yr_max+1))
+        self.cpi["cpi"].fillna(infl, inplace=True)
 
         # compute cumulative CPI
         self.cpi["cpi_index"] = ""
         self.cpi["cpi_index"] = \
             pd.to_numeric(self.cpi.cpi_index, errors="coerce")
-        self.cpi.loc[self.pl] = 1.0
+        self.cpi.loc[self.pl, "cpi_index"] = 1.0
         ix = self.cpi.index.get_loc(self.pl)
 
         # backward
         for i in range(ix-1, -1, -1):
             self.cpi.iloc[i]["cpi_index"] = \
-                self.cpi.iloc[i+1].cpi_index * self.cpi.iloc[i].cpi2
+                self.cpi.iloc[i+1].cpi_index * (self.cpi.iloc[i].cpi + 1.0)
         
         # forward
         for i in range(ix+1, len(self.cpi)):
             self.cpi.iloc[i]["cpi_index"] = \
-                self.cpi.iloc[i-1].cpi_index * self.cpi.iloc[i].cpi2
-
-        self.cpi.drop(columns=["cpi2"], inplace=True)
-
-        if verbose:
-            print(self.cpi.head(10))
+                self.cpi.iloc[i-1].cpi_index * (self.cpi.iloc[i-1].cpi + 1.0)
 
 
     def clean_data(self, verbose=False):
@@ -146,39 +141,50 @@ class DataContainer(object):
 
     def adjust_price_level(self, verbose=False):
         """Unify the prices for one price level"""
-        for c in ["c_op", "vtts", "voc", "c_fuel", "c_acc", "c_gg", "c_em", "noise"]:
+        if verbose:
+            print("Adjusting price level...")
+        for c in ["c_op", "vtts", "voc", "c_fuel", "c_acc", "c_gg", "c_em",\
+            "noise"]:
             if verbose:
                 print("Adjusting: %s" % c)
             self.df_clean[c]["value"] = self.df_clean[c].value \
                 * self.df_clean[c].price_level\
                 .map(lambda x: self.cpi.loc[x].cpi_index)
             self.df_clean[c].drop(columns=["price_level"], inplace=True)
-            self.df_clean[c]["value"] = self.df_clean[c].value.round(3)
+            self.df_clean[c]["value"] = self.df_clean[c].value.round(2)
 
 
-    def wrangle_data(self):
-        self._wrangle_vtts()
-        self._wrangle_fuel()
-        self._wrangle_accidents()
-        self._wrangle_emissions()
-        self._wrangle_noise()
+    def wrangle_data(self, *args, **kwargs):
+        self._wrangle_opex(*args, **kwargs)
+        self._wrangle_vtts(*args, **kwargs)
+        self._wrangle_fuel(*args, **kwargs)
+        self._wrangle_accidents(*args, **kwargs)
+        self._wrangle_emissions(*args, **kwargs)
+        self._wrangle_noise(*args, **kwargs)
 
 
     def _wrangle_opex(self, verbose=False):
-        """Set index and add conversion factors"""
+        """Set up index"""
         c = "c_op"
-        self.df_clean[c]["conv_fac"] = where(
-            self.df_clean[c].operation_type == "periodic", 
-            self.df_clean["conv_fac"].loc["periodic"]["aggregate"],
-            self.df_clean["conv_fac"].loc["routine"]["aggregate"])
-        self.df_clean[c]["value_eco"] = \
-            self.df_clean[c].value * self.df_clean[c].conv_fac
-
-        self.df_clean[c].rename(columns={"value": "value_fin"}, inplace=True)
-        
         self.df_clean[c].reset_index(inplace=True)
         self.df_clean[c].set_index(\
-            ["item","operation_type","section_type"], inplace=True)
+            ["item","operation_type","category"], inplace=True)
+
+#        """Set index and add conversion factors"""
+#        c = "c_op"
+#        self.df_clean[c]["conv_fac"] = where(
+#            self.df_clean[c].operation_type == "periodic", 
+#            self.df_clean["conv_fac"].loc["periodic"]["aggregate"],
+#            self.df_clean["conv_fac"].loc["routine"]["aggregate"])
+#        self.df_clean[c]["value_eco"] = \
+#            (self.df_clean[c].value * self.df_clean[c].conv_fac).round(2)
+#
+#        self.df_clean[c].rename(columns={"value": "value_fin"}, inplace=True)
+#        
+#        self.df_clean[c].reset_index(inplace=True)
+#        self.df_clean[c].set_index(\
+#            ["item","operation_type","category"], inplace=True)
+#        self.df_clean[c].drop(columns="conv_fac", inplace=True)
 
 
     def _wrangle_vtts(self, verbose=False):
@@ -186,7 +192,8 @@ class DataContainer(object):
         if "distance" in self.df_clean["vtts"].columns:
             if verbose:
                 print("Contracting distance.")
-            gr = self.df_clean["vtts"].groupby(by=["vehicle","substance","purpose",\
+            gr = self.df_clean["vtts"]\
+                .groupby(by=["vehicle","substance","purpose",\
                 "gdp_growth_adjustment"])
             vtts = gr["value"].mean()
             vtts = vtts.reset_index()
@@ -202,14 +209,16 @@ class DataContainer(object):
         self.df_clean["occ_p"]["substance"] = "passengers"
         self.df_clean["occ_p"].reset_index(inplace=True)
         
-        vtts = pd.merge(vtts, self.df_clean["occ_p"][["vehicle","value","substance"]],
-             how="left", on=["vehicle","substance"], suffixes=("", "_occ"))
+        vtts = pd.merge(vtts, \
+            self.df_clean["occ_p"][["vehicle","value","substance"]],
+            how="left", on=["vehicle","substance"], suffixes=("", "_occ"))
 
         # add freight occupancy
         self.df_clean["occ_f"]["substance"] = "freight"
         self.df_clean["occ_f"].reset_index(inplace=True)
-        vtts = pd.merge(vtts, self.df_clean["occ_f"][["vehicle","value","substance"]],
-             how="left", on=["vehicle","substance"], suffixes=("", "_freight"))
+        vtts = pd.merge(vtts, 
+            self.df_clean["occ_f"][["vehicle","value","substance"]],
+            how="left", on=["vehicle","substance"], suffixes=("", "_freight"))
 
         vtts["value_occ"] = vtts.value_occ.fillna(vtts.value_freight)
         vtts.drop(columns=["value_freight"], inplace=True)
@@ -269,17 +278,17 @@ class DataContainer(object):
         
         # copy to the cost dataframe
         self.df_clean["c_acc"] = self.df_clean["r_acc"]\
-            [["lanes","label","area","value","gdp_growth_adjustment"]].copy()
+            [["lanes","label","environment","value","gdp_growth_adjustment"]].copy()
 
         self.df_clean["c_acc"].reset_index(inplace=True)
         self.df_clean["c_acc"].set_index(\
-            ["road_type","lanes","label","area"], inplace=True)
+            ["road_type","lanes","label","environment"], inplace=True)
 
 
     def _wrangle_emissions(self, verbose=False):
         b = "c_em"
         self.df_clean[b].reset_index()\
-            .set_index(["polluant","area"], inplace=True)
+            .set_index(["polluant","environment"], inplace=True)
 
 
     def _wrangle_noise(self, verbose=False):
@@ -290,46 +299,33 @@ class DataContainer(object):
 
         self.df_clean[c]["value2"] = self.df_clean[c].value\
             * self.df_clean[c].ratio
-        gr = self.df_clean[c].groupby(["vehicle","area","gdp_growth_adjustment"])
+        gr = self.df_clean[c]\
+            .groupby(["vehicle","environment","gdp_growth_adjustment"])
 
         self.df_clean[c] = gr["value2"].sum()
         self.df_clean[c] = self.df_clean[c].reset_index()
         self.df_clean[c].rename(columns={"value2": "value"}, inplace=True)
-        self.df_clean[c].set_index(["vehicle","area"], inplace=True)
+        self.df_clean[c].set_index(["vehicle","environment"], inplace=True)
 
 
-    def create_time_benefit_mat(self, y_start, N_yr, verbose=True):
-        """Define the time-cost matrices for each benefit"""
-        yrs = arange(y_start, y_start + N_yr)
-        for b in ["vtts", "voc", "c_acc", "c_em", "noise"]:
-            if verbose:
-                print("Creating: %s" % b)
-            self.TM[b] = pd.DataFrame(columns=yrs, index=self.df_clean[b].index)
-            self.TM[b][y_start] = self.df_clean[b].value
-            for yr in yrs[1:]:
-                # CHECK INFLATION INDEXING
-                self.TM[b][yr] = self.TM[b][yr-1] #* self.cpi.loc[yr].cpi_index
-                if "gdp_growth_adjustment" in self.df_clean[b].columns:
-                    self.TM[b][yr] = self.TM[b][yr] \
-                    * (1.0 + self.gdp_growth.loc[yr].gdp_growth \
-                    * self.df_clean[b].gdp_growth_adjustment)
-
-        b = "gg"
-
-    
-    def create_time_opex_mat(self, y_start, N_yr, verbose=True):
-        c = "c_op"
-        yrs = arange(y_start, y_start + N_yr)
-
-        self.TM_fin[c] = pd.DataFrame(columns=yrs, index=self.df_clean[c].index)
-        self.TM_fin[c][y_start] = self.df_clean[c].value_fin
-        for yr in yrs[1:]:
-            self.TM_fin[c][yr] = self.TM_fin[c][yr-1] * self.cpi.loc[yr].cpi_index
-
-        self.TM[c] = pd.DataFrame(columns=yrs, index=self.df_clean[c].index)
-        self.TM[c][y_start] = self.df_clean[c].value_eco
-        for yr in yrs[1:]:
-            self.TM[c][yr] = self.TM[c][yr-1] * self.cpi.loc[yr].cpi_index
+#    def create_time_benefit_mat(self, y_start, N_yr, verbose=True):
+#        """Define the time-cost matrices for each benefit"""
+#        yrs = arange(y_start, y_start + N_yr)
+#        for b in ["vtts", "voc", "c_acc", "c_em", "noise"]:
+#            if verbose:
+#                print("Creating: %s" % b)
+#            self.TM[b] = pd.DataFrame(columns=yrs, index=self.df_clean[b].index)
+#            self.TM[b][y_start] = self.df_clean[b].value
+#            for yr in yrs[1:]:
+#                self.TM[b][yr] = self.TM[b][yrs[0]] * self.cpi.loc[yr].cpi_index
+#                if "gdp_growth_adjustment" in self.df_clean[b].columns:
+#                    self.TM[b][yr] = self.TM[b][yr] \
+#                    * (1.0 + self.gdp_growth.loc[yr].gdp_growth \
+#                    * self.df_clean[b].gdp_growth_adjustment)
+#
+#        b = "gg"
+#        # CREATE GREENHOUSE TIME MATRIX
+#
 
 
 

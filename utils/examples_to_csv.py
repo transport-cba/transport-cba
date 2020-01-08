c0e3b1295c22324c29821ab022c59d8eed427723
petervanya <peter.vanya@gmail.com>
2020-01-08 20:25:22 +0100

    Initial commit

---
+++ utils/examples_to_csv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+Read in the excel and transform each sheet into a csv
+in the production folder.
+
+18/12/19
+"""
+import pandas as pd
+
+
+dirn = "../examples/"
+
+fname = dirn + "example_bypass.xlsx"
+xls = pd.ExcelFile(fname)
+print("File name: %s" % fname)
+print("Sheet names:", xls.sheet_names)
+
+
+for name in xls.sheet_names:
+    df = xls.parse(name)
+    print("Saving %s..." % name)
+    df.to_csv(dirn + "bypass_" + name + ".csv", index=False)

cb43eb852dacdd91519fcb352e1b128c59bdcbc0
petervanya <peter.vanya@gmail.com>
2023-01-08 15:34:21 +0100

    added pkg version and output

--- tests/test_road_cba.py
+++ tests/test_road_cba.py
@@ -2,8 +2,14 @@
 from pycba import RoadCBA
 from pycba.sample_projects import load_sample_bypass
 
+import numpy as np
+import pandas as pd
+print(f"numpy: {np.__version__}")
+print(f"pandas: {pd.__version__}")
+
 b = load_sample_bypass()
 
 cba = RoadCBA(2020, 2020, "svk", verbose=True)
 cba.read_project_inputs(*b.values())
-cba.economic_analysis()
+res = cba.economic_analysis()
+print(res)

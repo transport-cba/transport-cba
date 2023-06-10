978e76d3aaecfe4296bd99dcfd411770a09a1954
petervanya <peter.vanya@gmail.com>
2023-06-10 16:55:30 +0200

    added future tests

--- tests/test_road_cba.py
+++ tests/test_road_cba.py
@@ -5,22 +5,50 @@ from pycba.sample_projects import load_sample_bypass
 
 import numpy as np
 import pandas as pd
-print(f"numpy: {np.__version__}")
-print(f"pandas: {pd.__version__}")
 
+print(f"numpy version: {np.__version__}")
+print(f"pandas version: {pd.__version__}")
+print(f"pycba version: {pycba.__version__}")
 print(f"Source for testing: {pycba.__file__}")
 
-b = load_sample_bypass()
 
-cba = RoadCBA(2020, "svk", verbose=True)
-cba.read_project_inputs(*b.values())
+def test_whole_cba_pipeline():
+    """Test whole pipeline"""
+    b = load_sample_bypass()
+    
+    cba = RoadCBA(2020, "svk", verbose=True)
+    cba.read_project_inputs(*b.values())
+    
+    # compute cba
+    res = cba.economic_analysis()
+    print(res)
+    
+    # save outputs
+    cba.save_results_to_excel()
 
-# test saving inputs
-cba.save_inputs_to_excel()
+    assert res.shape == (3, 3) and res["Value"].isna().sum() == 0
 
-# compute cba
-res = cba.economic_analysis()
-print(res)
+def test_real_cba_excels():
+    pass
 
-# save outputs
-cba.save_results_to_excel()
+def test_input_saving():
+    b = load_sample_bypass()
+    
+    cba = RoadCBA(2020, "svk", verbose=True)
+    cba.read_project_inputs(*b.values())
+    
+    # test saving inputs
+    cba.save_inputs_to_excel()
+    assert True
+
+def test_sheet_names():
+    pass
+
+def test_consistency_year_ranges():
+    pass
+
+def test_road_param_cols():
+    pass
+
+def test_capex_cols():
+    pass
